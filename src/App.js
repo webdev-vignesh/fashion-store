44424ae163e8d75993c81d35d5d8063dc9fa1903
web3 vicky <vicky.web3@gmail.com>
2022-10-16 20:55:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import Layout from './Routes/Layout';
+import Route from './Routes/Route';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout />
+      <Route />
+    </>
   );
 }
 
